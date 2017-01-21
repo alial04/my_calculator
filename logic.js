024e3bf59f2df5c7053d9b31c3ccef2f133cb0ff
F4uzan <f4uzan@this.is.f4>
2017-01-21 21:28:44 +0700

    logic: Nicer styling
    PLEASE DON'T BLAME ME IF THIS ISN'T YOUR STYLE

--- logic.js
+++ logic.js
@@ -1,28 +1,24 @@
 var box=document.getElementById('display');
 
-function addtoscreen(x)
-{
+function addtoscreen(x) {
     box.value +=x;
     if(x=='C') { box.value=''; }
 }
 
-function answer()
-{
+function answer() {
     x=box.value;
     x=eval(x);
     box.value =x;
 }
 
-function backspace()
-{
+function backspace() {
     var number=box.value;
     var len=number.length-1;
     var newnumber=number.substring(0,len)
     box.value=newnumber;
 }
 
-function power(y)
-{
+function power(y) {
     x=box.value;
     x=Math.pow(x,y);
     box.value=x;
