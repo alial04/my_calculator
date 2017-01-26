d7f8d9d373ce0adb9eda1e58ac7ac07e184fdf81
F4uzan <f4uzan@this.is.f4>
2017-01-26 11:55:40 +0700

    calculator: Design overhaul
    Update the design to match Material Design, I'll revamp some of the design later, but this'll do for now.
    
    This may cause some commits to revert, be careful when merging!

--- JS/logic.js
+++ JS/logic.js
@@ -1,24 +1,36 @@
 var box=document.getElementById('display');
 
-function addtoscreen(x) {
+function addtoscreen(x)
+{
     box.value +=x;
-    if(x=='C') { box.value=''; }
+
+    if(x=='C')
+    {
+       box.value='';
+    }
 }
 
-function answer() {
+function answer()
+{
     x=box.value;
-    x=eval(x);
-    box.value =x;
+	if (!x && x == "undefined") {
+		x=eval(x);
+		box.value =x;
+	} else {
+		box.value ='0';
+	}
 }
 
-function backspace() {
+function backspace()
+{
     var number=box.value;
     var len=number.length-1;
     var newnumber=number.substring(0,len)
     box.value=newnumber;
 }
 
-function power(y) {
+function power(y)
+{
     x=box.value;
     x=Math.pow(x,y);
     box.value=x;
