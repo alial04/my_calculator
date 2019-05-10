4c1c318797bc200a64edfc7d36d26541d05a4a59
Ali Al-Asadi <alasadi56@gmail.com>
2019-05-11 00:29:38 +0200

    Revert "calculator: Design overhaul"
    This reverts commit d7f8d9d373ce0adb9eda1e58ac7ac07e184fdf81.

--- logic.js
+++ logic.js
@@ -1,36 +1,24 @@
 var box=document.getElementById('display');
 
-function addtoscreen(x)
-{
+function addtoscreen(x) {
     box.value +=x;
-
-    if(x=='C')
-    {
-       box.value='';
-    }
+    if(x=='C') { box.value=''; }
 }
 
-function answer()
-{
+function answer() {
     x=box.value;
-	if (!x && x == "undefined") {
-		x=eval(x);
-		box.value =x;
-	} else {
-		box.value ='0';
-	}
+    x=eval(x);
+    box.value =x;
 }
 
-function backspace()
-{
+function backspace() {
     var number=box.value;
     var len=number.length-1;
     var newnumber=number.substring(0,len)
     box.value=newnumber;
 }
 
-function power(y)
-{
+function power(y) {
     x=box.value;
     x=Math.pow(x,y);
     box.value=x;
